a00a0158cad964b3d9f13566ef928f8421b642f0
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-16 22:19:19 +0000

    build based on d73d281

---
+++ latest/search_index.js
@@ -0,0 +1,107 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#JuliaPackageTemplate.jl-Documentation-1",
+    "page": "Home",
+    "title": "JuliaPackageTemplate.jl Documentation",
+    "category": "section",
+    "text": "Welcome to the JuliaPackageTemplate.jl documentation. The prurpose of this  project is to provide an example package structure which can be easily copied and modified."
+},
+
+{
+    "location": "#Making-A-Custom-Package-Logo-1",
+    "page": "Home",
+    "title": "Making A Custom Package Logo",
+    "category": "section",
+    "text": "To make a custom package logo and include it in the documentation, simply add a  logo.png file to /docs/assets/. For the logo to render well, it is recommended to be 500px by 500px and at least 144dpi."
+},
+
+{
+    "location": "modules/submodule/#",
+    "page": "Submodule",
+    "title": "Submodule",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "modules/submodule/#JuliaPackageTemplate.YourSubmodule.your_function",
+    "page": "Submodule",
+    "title": "JuliaPackageTemplate.YourSubmodule.your_function",
+    "category": "function",
+    "text": "Write something about the purpose of the function up top.\n\nArguments:\n\narg_1::Integer Desribute first function argument. [input_units]\narg_2::Integer Desribute second function argument. [input_units]\n\nReturns:\n\nret_1::Float64 Describe first return value. [input_units]\nret_2::Float64 Describe second return value. [input_units]\n\nNotes:\n\nMake specific comments about corner cases or things to know about usage here.\n\nReferences:\n\nList citations to references used in writing the function here\n\n\n\n\n\n"
+},
+
+{
+    "location": "modules/submodule/#Submodule-1",
+    "page": "Submodule",
+    "title": "Submodule",
+    "category": "section",
+    "text": "Write Markdown with inline comments to create documentation pages.your_function"
+},
+
+{
+    "location": "modules/rubber_ducks/#",
+    "page": "Rubber Ducks",
+    "title": "Rubber Ducks",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "modules/rubber_ducks/#JuliaPackageTemplate.RubberDucks.rubber_ducks_in_earth",
+    "page": "Rubber Ducks",
+    "title": "JuliaPackageTemplate.RubberDucks.rubber_ducks_in_earth",
+    "category": "function",
+    "text": "Computes the number of rubber ducks that would fill the volume of the Earth.\n\nAssumes a spherical Earth, and liquid rubber ducks (to avoid the packing problem).\n\nArguments:\n\nnum_ruber::Real is the first function argument. [m^3]\nr_earth::Real is the first function argument. Default: 6378000 [m]\n\nReturns:\n\nnum_ducks::Float64 Number of rubber ducks that could \n\n\n\n\n\n"
+},
+
+{
+    "location": "modules/rubber_ducks/#JuliaPackageTemplate.RubberDucks.RUBBER_DUCK_PER_SEC",
+    "page": "Rubber Ducks",
+    "title": "JuliaPackageTemplate.RubberDucks.RUBBER_DUCK_PER_SEC",
+    "category": "constant",
+    "text": "Number of rubber ducks products per second in the entire world on average.\n\nUnits: ducks/sec\n\n\n\n\n\n"
+},
+
+{
+    "location": "modules/rubber_ducks/#Rubber-Ducks-1",
+    "page": "Rubber Ducks",
+    "title": "Rubber Ducks",
+    "category": "section",
+    "text": "You can modify the name of both the file in the documentationrubber_ducks_in_earth\nRUBBER_DUCK_PER_SEC"
+},
+
+{
+    "location": "examples/plotting_example/#",
+    "page": "Plotting Example",
+    "title": "Plotting Example",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "examples/plotting_example/#Plotting-Example-1",
+    "page": "Plotting Example",
+    "title": "Plotting Example",
+    "category": "section",
+    "text": "In this example we showcase how to automatically build and generate plots as part of the documentation deployment process.The plots in this example are produced by the file docks/src/makeplots.jl The contents of that file are run each time the documentation is deployed. (Image: )note: Note\nPlots will only appear in documentation when deployed to github pages.warning: Orange Box\nHere is how you add warning box to your documentation"
+},
+
+{
+    "location": "library_index/#",
+    "page": "Librrary Index",
+    "title": "Librrary Index",
+    "category": "page",
+    "text": ""
+},
+
+]}
